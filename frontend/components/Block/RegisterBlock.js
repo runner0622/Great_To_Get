4f2f2daacb20d22d373e0c11397ad206c9b24c34
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2022-10-16 22:45:23 +0530

    added register

---
+++ frontend/components/Block/RegisterBlock.js
@@ -0,0 +1,129 @@
+import { useState } from "react";
+import { url } from "../../helper";
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffectAsync } from "../../helper";
+import hasher from 'js-sha256';
+import customToast from "./swal/customToast";
+
+const RegisterBlock = (props) => {
+    const router = useRouter();
+    const [path, setPATH] = useState('');
+
+    useEffectAsync(() => {
+        if (!router.isReady)
+            return;
+
+        setPATH(router.asPath?.split('/')?.pop())
+
+
+    }, [router.isReady])
+
+    // const history = useHistory();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+
+    const usernameChangeHandler = ({ target: { value } }) => {
+        setUsername(value);
+    };
+    const passwordChangeHandler = ({ target: { value } }) => {
+        setPassword(value);
+    };
+    const emailChangeHandler = ({ target: { value } }) => {
+        setEmail(value);
+    };
+
+    const registerHandler = async () => {
+        // e.preventDefault();
+
+        try {
+            localStorage.clear();
+            const loginResult = await axios.post(
+                url("/auth/user/register"),
+                {
+                    username: username,
+                    password: hasher.sha256(password).toString(),
+                    email: email
+                }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+
+
+            if (
+                loginResult.status === 200 &&
+                loginResult.data.msg === "success") {
+
+
+                // redirect to homepage
+                router.push('/login')
+
+                customToast(
+                    "success",
+                    "User Registration Successfilly"
+                );
+            }
+        } catch (error) {
+            customToast(
+                "warning",
+                "Login Failed"
+            );
+        }
+    };
+
+    return (
+        <div className="loginwrapper">
+            <div className="centerwrapper">
+                <div className="centerwrapper__section centerwrapper__adminlogin">
+                    {
+                        path === 'checkout' ?
+                            "Please login to checkout products" :
+                            "Login User"
+                    }
+                </div>
+                <div className="centerwrapper__section">
+                    <input
+                        type="email"
+                        onChange={emailChangeHandler}
+                        placeholder="Email"
+                        className="centerwrapper__section__input"
+                        required={true}
+                    />
+                </div>
+                <div className="centerwrapper__section">
+                    <input
+                        type="text"
+                        onChange={usernameChangeHandler}
+                        placeholder="Username"
+                        className="centerwrapper__section__input"
+                        required={true}
+                    />
+                </div>
+                <div className="centerwrapper__section">
+                    <input
+                        type="password"
+                        onChange={passwordChangeHandler}
+                        placeholder="Password"
+                        className="centerwrapper__section__input"
+                        required={true}
+                    />
+                </div>
+                <div className="centerwrapper__section">
+                    <button
+                        className="centerwrapper__section__button"
+                        type="submit"
+                        value="submit"
+                        onClick={registerHandler}
+                    >
+                        Register
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterBlock;
---
+++ frontend/pages/register.js
@@ -0,0 +1,9 @@
+import RegisterBlock from "../components/Block/RegisterBlock";
+
+const RegisterPage = () => {
+    return (
+        <RegisterBlock />
+    )
+}
+
+export default RegisterPage
